b75aa6627ff863a45301110d7175e786bd350b99
Tenmayos <tenmayos_2@hotmail.com>
2022-12-31 12:06:50 +0300

    Wrapped 'img' inside a 'header' inside 'MainLogo' component for clarification

--- src/components/MainLogo.jsx
+++ src/components/MainLogo.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function MainLogo() {
     return (
+    <header>
     <img className="center" src="/images/logo-color.png" alt="Alrouf Company Logo"/>
+    </header>
     );
 }
 
