41df5531d3c2ec233967ca21026d3367d5b8579f
Tenmayos <tenmayos_2@hotmail.com>
2022-12-31 14:06:57 +0300

    Added the 'ApplicationBox' component to the main 'App'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import MainLogo from "./MainLogo.jsx";
 import DescriptionBox from "./DescriptionBox.jsx";
+import ApplicationBox from "./ApplicationBox.jsx";
 
 function App() {
-
+    
     return (
         <div>
             <MainLogo />
             <DescriptionBox />
+            <br />
+            <ApplicationBox />
         </div>
     );
 }
