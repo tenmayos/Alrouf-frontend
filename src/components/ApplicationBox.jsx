3cdf6ef0ccdd5f61f7017f3ec17a6aec884a8d2f
Tenmayos <tenmayos_2@hotmail.com>
2022-12-31 18:59:07 +0300

    removed `identifierText` for a simpler interface-look  | Added multiple `TextInputSlice` components and gave them an in-line style object  | Added and styled a `label`, `input type=file`  | Added states and handlers to capture the uploaded PDF file | Finalized the overall ApplicationBox Appearance and very basic functionality

--- src/components/ApplicationBox.jsx
+++ src/components/ApplicationBox.jsx
@@ -3,26 +3,71 @@ import TextInputSlice from "./TextInputSlice";
 
 function ApplicationBox() {
 
+    const styleObj = {
+        "marginTop": "2%",
+        "marginBottom": "2%"
+    }
+    const [isMouseOn, setIsMouseOn] = React.useState(false);
+    const [pdfResume, setPdfResume] = React.useState(null);
+
+    function HandleMouseHover() {
+        isMouseOn? setIsMouseOn(false) : setIsMouseOn(true);
+    }
+
+    function HandleFileInput(event) {
+        let pdfFile = event.target.files[0];
+        setPdfResume(pdfFile);
+    }
+
     return (
         <div className="submission-box">
             <form>
                 <TextInputSlice
-                    placeholder="الاسم الاول هنا"
-                    identifierText="الاسم الاول"
+                    style={styleObj}
+                    placeholder="الاسم الاول"
+                />
+                <TextInputSlice
+                    style={styleObj}
+                    placeholder="اسم الأب"
+                />
+                <TextInputSlice
+                    style={styleObj}
+                    placeholder="اسم العائلة"
+                />
+                <TextInputSlice
+                    style={styleObj}
+                    placeholder="جامعة التخرج"
+                />
+                <TextInputSlice
+                    style={styleObj}
+                    placeholder="التخصص العلمي"
+                />
+                <TextInputSlice
+                    style={styleObj}
+                    placeholder="المعدل الجامعي"
+                />
+                <TextInputSlice
+                    style={styleObj}
+                    placeholder="المعدل الكلي"
+                />
+                <TextInputSlice
+                    style={styleObj}
+                    placeholder="رقم الهاتف"
                 />
                 <br />
-                <TextInputSlice
-                    placeholder="اسم الأب هنا"
-                    identifierText="اسم الأب"
-                />
-                <br />
-                <TextInputSlice
-                    placeholder="اسم العائلة هنا"
-                    identifierText="اسم العائلة"
-                />
+
+                <label
+                style={isMouseOn? {"background": "red"} : null}
+                onMouseEnter={HandleMouseHover}
+                onMouseLeave={HandleMouseHover}
+                htmlFor="file-upload"
+                className="file-upload-button">
+                    {pdfResume === null? "ارفق السيرة الذاتية" : pdfResume.name}
+                </label>
+                <input id="file-upload" onChange={HandleFileInput} type="file" accept=".pdf" />
             </form>
         </div>
-    )
+    );
 }
 
 export default ApplicationBox;
