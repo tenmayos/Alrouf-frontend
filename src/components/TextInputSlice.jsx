26b9ec916b996ec60062d50de57cf0737c6f80f2
Tenmayos <tenmayos_2@hotmail.com>
2023-01-01 16:17:27 +0300

    Removed the  method as it didn't provide the desired results, and instead the main calling component can handle its changes and values, added an uid=197609(Tenmayos) gid=197121 groups=197121 field for a more accurate data recognition

--- src/components/TextInputSlice.jsx
+++ src/components/TextInputSlice.jsx
@@ -2,20 +2,16 @@ import React from "react";
 
 function TextInputSlice(props) {
 
-    const [textValue, setTextValue] = React.useState("");
-
-    function HandleChange(event) {
-        const newTextValue = event.target.value;
-        setTextValue(newTextValue);
-    }
-
     return (
         <div>
         <input
         style={props.style}
         className="text-input"
-        type="text" onChange={HandleChange}
-        placeholder={props.placeholder} value={textValue}
+        id={props.id}
+        onChange={(e) => {
+            props.onChange(e)
+            }}
+        placeholder={props.placeholder}
         />
         </div>
     );
